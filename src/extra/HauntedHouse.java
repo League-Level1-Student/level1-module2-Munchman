8a86e6e259005a89f7db6c0da35a6085098e6700
League <League@AdministorsiMac.attlocal.net>
2018-09-10 18:32:03 -0700

    Haunted house

---
+++ src/extra/HauntedHouse.java
@@ -0,0 +1,28 @@
+package extra;
+
+public class HauntedHouse {
+	int skeletons; 
+    String name; 
+
+    HauntedHouse(String name, int skeletons) { 
+         name = name; 
+         skeletons = skeletons; 
+    } 
+
+    void doScaryStuff(int pumpkins, String scaryPhrase) { 
+         System.out.println(pumpkins + " pumpkins say " + scaryPhrase); 
+    } 
+
+    boolean isSpooky() { 
+         //Haunted houses are spooky! 
+         return true; 
+    } 
+
+   public static void main(String[] args) {
+	
+ 
+         HauntedHouse h = new HauntedHouse("SpookyZone", 105); 
+         h.doScaryStuff(1000000, "Boo!"); 
+         h.isSpooky(); 
+     
+   }}
